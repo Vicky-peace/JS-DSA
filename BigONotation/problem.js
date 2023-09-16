e4394cfa2318373b7d6599d1cadb393a23b1bea9
Vicky-peace <victorpeace47@gmail.com>
2023-09-16 12:17:12 +0300

    enough of dsa today

--- BigONotation/problem.js
+++ BigONotation/problem.js
@@ -43,21 +43,33 @@
 
 // refactored solution
 
+// function charCount(str) {
+//   let obj = {};
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i].toLowerCase();
+//     if (/[a-z0-9]/.test(char)) {
+//       if (obj[char] > 0) {
+//         obj[char]++;
+//       } else {
+//         obj[char] = 1;
+//         1;
+//       }
+//     }
+//   }
+//   return obj;
+// }
+// console.log(
+//   charCount("Hello how are you doing, this is my new number 09&2234%")
+// );
+
 function charCount(str) {
   let obj = {};
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i].toLowerCase();
+  for (let char of str) {
+    char = char.toLowerCase();
     if (/[a-z0-9]/.test(char)) {
-      if (obj[char] > 0) {
-        obj[char]++;
-      } else {
-        obj[char] = 1;
-        1;
-      }
+      obj[char] = ++obj[char] || 1;
     }
   }
   return obj;
 }
-console.log(
-  charCount("Hello how are you doing, this is my new number 09&2234%")
-);
+console.log(charCount("Hello there )&14933112"));
