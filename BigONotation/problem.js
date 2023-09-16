9541b731c79c7a2db2cdbd76621ce07c90c24695
Vicky-peace <victorpeace47@gmail.com>
2023-09-16 11:50:11 +0300

    refactored solution

--- BigONotation/problem.js
+++ BigONotation/problem.js
@@ -19,24 +19,45 @@
 // const result = countCharacters(inputString);
 // console.log(result);
 
-function charCount(str) {
-  // make an object to return at the end
-  let result = {};
+// function charCount(str) {
+//   // make an object to return at the end
+//   let result = {};
 
-  // loop over string , for each character...
+//   // loop over string , for each character...
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i].toLowerCase();
+//     //if the char is a number/letter AND is a key in object, add one to count
+//     if (result[char] > 0) {
+//       result[char]++;
+//       // if thhe char is a number/letter AND not in object, add it to object and set value to 1
+//     } else {
+//       result[char] = 1;
+//     }
+//   }
+//   // if the character is something else (space , period etc) dont do anything
+//   // return object data at the end
+//   return result;
+// }
+
+// console.log(charCount("Hi there nigga"));
+
+// refactored solution
+
+function charCount(str) {
+  let obj = {};
   for (let i = 0; i < str.length; i++) {
     let char = str[i].toLowerCase();
-    //if the char is a number/letter AND is a key in object, add one to count
-    if (result[char] > 0) {
-      result[char]++;
-      // if thhe char is a number/letter AND not in object, add it to object and set value to 1
-    } else {
-      result[char] = 1;
+    if (/[a-z0-9]/.test(char)) {
+      if (obj[char] > 0) {
+        obj[char]++;
+      } else {
+        obj[char] = 1;
+        1;
+      }
     }
   }
-  // if the character is something else (space , period etc) dont do anything
-  // return object data at the end
-  return result;
+  return obj;
 }
-
-console.log(charCount("Hi there nigga"));
+console.log(
+  charCount("Hello how are you doing, this is my new number 09&2234%")
+);
